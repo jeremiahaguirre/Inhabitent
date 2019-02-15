38b4d2bef53341acbe5b326f0fde5fa4dd8fe931
Jeremiah <happytobike@gmail.com>
2019-02-15 13:48:11 -0800

    Initial commit

---
+++ themes/Inhabitent/footer.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					
+					<i class="fas fa-phone"></i>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
